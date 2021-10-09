a5d099bb19bee50c6e0b1c539181f271b761ec53
JuliaSarsunova <92164747+JuliaSarsunova@users.noreply.github.com>
2021-10-09 18:40:14 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -1,9 +1,9 @@
 function checkTemp(temperatuur){
     if (temperatuur <= 20){
         console.log("liiga kulm")}
-    if (temperatuur >= 21 && arv1 <= 40){
+    if (temperatuur >= 21 && temperatuur <= 40){
         console.log("paras temperatuur")}
-    if (temperatuur >60){
+    if (temperatuur >= 41){
         console.log("liiga kuum")}
 }
 
@@ -11,10 +11,10 @@ function checkTemp(temperatuur){
     if (temperatuur <= 20){
         console.log("liiga kulm")
         return (-1)}
-    if (temperatuur >= 21 && arv1 <= 40){
+    if (temperatuur >= 21 && temperatuur <= 40){
         console.log("paras temperatuur")
         return(0)}
-    if (temperatuur >60){
+    if (temperatuur > 41){
         console.log("liiga kuum")
         return(1)}
 }
