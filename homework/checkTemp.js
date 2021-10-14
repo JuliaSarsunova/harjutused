35b5b02e02a2b929df5490fa5e856d64b9203c54
JuliaSarsunova <92164747+JuliaSarsunova@users.noreply.github.com>
2021-10-14 17:27:04 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -14,7 +14,7 @@ function checkTemp(temperatuur){
     if (temperatuur >= 21 && temperatuur <= 40){
         console.log("paras temperatuur")
         return(0)}
-    if (temperatuur > 41){
+    if (temperatuur >= 41){
         console.log("liiga kuum")
         return(1)}
 }
